b0cf13139f38b2fa856fd1c30ef547427e85ee73
Kyle Shrote <kyle.shrote@gmail.com>
2018-07-15 21:50:38 -0400

    package changes

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,5 @@ router.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello From Express' });
 });
 
-// If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-router.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-  });
-
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    router.use(express.static(path.join(__dirname, './client/build')));
-    // Handle React routing, return all requests to React app
-    router.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-    });
-  }
-
 
 module.exports = router;
