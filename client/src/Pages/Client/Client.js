978e8df361716dd5356251fb0273b1ebdb96f7a3
Kyle Shrote <kyle.shrote@gmail.com>
2018-07-19 20:20:02 -0400

    added sidebar

---
+++ client/src/Pages/Client/Client.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Client.css'
+
+class Client extends Component {
+  render() {
+    return (
+        <div className="client">
+            <h1 className="App-title">Heres your client overview</h1>
+            <p className="App-intro">:p</p>
+        </div>
+    );
+  }
+}
+
+export default Client;
--- client/src/Pages/Dashboard/Dashboard.js
+++ client/src/Pages/Dashboard/Dashboard.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import NewProject from '../../Components/Modals/NewProject'
 import './Dashboard.css'
+import Sidebar from '../../Components/Sidebar'
 
 
 class Dashboard extends Component {
   render() {
     return (
-        <div className="Dashboard">
-            <div>
-              <NewProject />
-            </div>
-            <h1 className="App-title">Welcome to Your Dashboard</h1>
-            <p className="App-intro">Wondering how to get started??</p>
-            <img src="https://media.giphy.com/media/AuQm5kCePSfiE/giphy.gif" alt="Giphy" />
-        </div>
+      <div className="Dashboard">
+        <Sidebar />
+        <NewProject />
+      </div>  
     );
   }
 }
