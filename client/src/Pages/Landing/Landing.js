dd986dbd2e1c96fb0d73ec709292e473e03b9566
kyle shrote <kyle.shrote@gmail.com>
2018-07-20 00:08:45 -0400

    Update Landing.js

--- client/src/Pages/Landing/Landing.js
+++ client/src/Pages/Landing/Landing.js
@@ -17,21 +17,6 @@ import { FormControl} from 'react-bootstrap';
 class Landing extends Component {
   render() {
     return (
-
-      <div className="Landing">
-          <Jumbotron>
-            <h1>Say Goodbye to Spreadsheets</h1>
-            <p>
-              Generate the perfect keyword ideas and eliminate the need for tedious spreadsheets.
-            </p>
-            <img src="http://via.placeholder.com/550x450" class="img-fluid" alt="Responsive image"></img>
-            <p>
-              <Button bsStyle="primary">Get Started</Button>
-              <Button bsStyle="primary">Learn More</Button>
-            </p>
-          </Jumbotron>;
-        </div>
-
         <div className="Landing">
 
             <Jumbotron>
