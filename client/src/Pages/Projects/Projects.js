019c66cc81154ab09f13f87f1e36422d28170b5e
Leslie Hernandez <lesliehernandez@Leslies-MacBook-Pro.local>
2018-07-20 22:59:23 -0400

    added tables

--- client/src/Pages/Projects/Projects.js
+++ client/src/Pages/Projects/Projects.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './Projects.css'
+import Tables from '../../Components/Tables/Table'
 
 class Projects extends Component {
   render() {
     return (
         <div className="Projects">
             <h1 className="App-title">Here are your projects</h1>
+
+<Tables />
             <p className="App-intro">...</p>
         </div>
     );
