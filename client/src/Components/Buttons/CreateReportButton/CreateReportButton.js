f79d3cba591e14b0ccde7ce505adfe9f1f38d149
Kyle Shrote <kyle.shrote@gmail.com>
2018-07-25 00:18:39 -0400

    We Routed

--- client/src/Components/Buttons/CreateReportButton/CreateReportButton.js
+++ client/src/Components/Buttons/CreateReportButton/CreateReportButton.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import { Button } from '@material-ui/core';
+import { withRouter } from 'react-router-dom'
 
 
 
-class CreateReportButton extends Component {
 
-    render(){
-        return(
-            <Button variant="contained" style={{margin: '0', float: 'right', backgroundColor: '#46E4C4'}}>
-                <Link to={`/reports/reportsBuilder/${this.props.projectId}`} style={{height: '15px', padding: '0', color: 'rgba(0, 0, 0, 0.87)', fontSize: '0.875rem', minWidth: '150px',
-    minHeight: '25px',fontFamily: 'Roboto', borderRadius: '4px', textTransform: 'uppercase', verticalAlign: 'middle', paddingTop: '7px', backgroundColor: '#46E4C4'}}>
-                Create Report
-                </Link>
-            </Button>
-        )
-    }
-}
+// this also works with react-router-native
+
+const CreateReportButton = withRouter(({ history, ...props }) => (
+  <Button
+    variant="contained"
+    onClick={() => { history.push(`/dashboard/buildreport/${props.projectId}`) }}
+    style={{
+        margin: '0', 
+        float: 'right', 
+        backgroundColor: '#46E4C4',
+        height: '15px',
+        padding: '0',
+        color: 'rgba(0, 0, 0, 0.87)', 
+        fontSize: '0.875rem', 
+        minWidth: '150px',
+        minHeight: '25px',
+        fontFamily: 'Roboto', 
+        borderRadius: '4px', 
+        textTransform: 'uppercase', 
+        verticalAlign: 'middle', 
+        paddingTop: '7px', 
+        backgroundColor: '#46E4C4'
+    }}
+  >
+    Create Report
+  </Button>
+))
+
+// class CreateReportButton extends Component {
+//     createReportHandler = () => {
+//         return(
+//             <Redirect to={`/dashboard/buildreport/${this.props.projectId}`} />
+//        )
+//     }
+//     render(){
+//         return(
+//             <Button 
+//                 onCLick={this.createReportHandler}
+//                 variant="contained" 
+//                 style={{
+//                     margin: '0', 
+//                     float: 'right', 
+//                     backgroundColor: '#46E4C4',
+//                     height: '15px',
+//                     padding: '0',
+//                     color: 'rgba(0, 0, 0, 0.87)', 
+//                     fontSize: '0.875rem', 
+//                     minWidth: '150px',
+//                     minHeight: '25px',
+//                     fontFamily: 'Roboto', 
+//                     borderRadius: '4px', 
+//                     textTransform: 'uppercase', 
+//                     verticalAlign: 'middle', 
+//                     paddingTop: '7px', 
+//                     backgroundColor: '#46E4C4'
+//                 }}
+//             >
+//             Create Report
+//             </Button>
+//         )
+//     }
+// }
 
 export default CreateReportButton;
--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,20 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Landing from './Pages/Landing';
 import App from './App';
 import Dashboard from './Pages/Dashboard';
+import Client from './Pages/Client';
+import ViewReports from './Pages/ViewReports';
+import ReportsBuilder from './Pages/ReportsBuilder';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Router>
         <div>
             <Route exact path="/landing" component={Landing} />
-            <Route exact path="/" component={App} />
-            <Route exact path="/dashboard" component={Dashboard} />
+            <Route path="/" component={App} />
+            <Route path="/dashboard" component={Dashboard} />
+            {/* <Route path="dashboard/client/" component={Client} />
+            <Route path="dashboard/reports/:id" component={ViewReports} />
+            <Route path="dashboard/reportbuilder/:id" component={ReportsBuilder} /> */}
         </div>
     </Router>,
     document.getElementById('root')
