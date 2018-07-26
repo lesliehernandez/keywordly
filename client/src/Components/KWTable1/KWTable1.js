8d304a89403233edeb73bce2475ae9b1af6c119c
Leslie Hernandez <lesliehernandez@Leslies-MacBook-Pro.local>
2018-07-25 22:04:46 -0400

    these damn tables

--- client/src/Components/KWTable1/KWTable1.js
+++ client/src/Components/KWTable1/KWTable1.js
@@ -1,323 +1,82 @@
 import React from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import { lighten } from '@material-ui/core/styles/colorManipulator';
-
-let counter = 0;
-function createData(name, calories, fat, carbs, protein) {
-  counter += 1;
-  return { id: counter, name, calories, fat, carbs, protein };
-}
-
-const columnData = [
-  { id: 'label', numeric: false, disablePadding: true, label: '' },
-  { id: 'rankcount', numeric: true, disablePadding: false, label: 'Rank Count' },
-  { id: 'svsum', numeric: true, disablePadding: false, label: 'Search Volume Sum' },
-  { id: 'avgcompetition', numeric: true, disablePadding: false, label: 'Average Competition' },
-  { id: 'uniqueurldensity', numeric: true, disablePadding: false, label: 'Unique URL Density' },
-  { id: 'trafficpercentage', numeric: true, disablePadding: false, label: 'Traffic %' },
-];
-
-class EnhancedTableHead extends React.Component {
-  createSortHandler = property => event => {
-    this.props.onRequestSort(event, property);
-  };
-
-  render() {
-    const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
-
-    return (
-      <TableHead>
-        <TableRow>
-        <br></br><br></br><h1 style= {{fontSize: '14px', fontFamily: 'Montserrat', fontWeight: '500'}}>Report Builder</h1><br></br><br></br>
-          {columnData.map(column => {
-            return (
-              <TableCell
-                key={column.id}
-                numeric={column.numeric}
-                padding={column.disablePadding ? '5px' : 'default'}
-              >
-              </TableCell>
-            );
-          }, this)}
-        </TableRow>
-      </TableHead>
-    );
-  }
-}
-
-EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.string.isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
-const toolbarStyles = theme => ({
-  root: {
-    paddingRight: theme.spacing.unit,
-  },
-  highlight:
-    theme.palette.type === 'light'
-      ? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-        }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
-  spacer: {
-    flex: '1 1 100%',
-  },
-  actions: {
-    color: theme.palette.text.secondary,
-  },
-  title: {
-    flex: '0 0 auto',
-  },
-});
-
-let EnhancedTableToolbar = props => {
-  const { numSelected, classes } = props;
-
-  return (
-    <Toolbar
-      className={classNames(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      <div className={classes.title}>
-        {numSelected > 0 ? (
-          <Typography color="inherit" variant="subheading">
-            {numSelected} selected
-          </Typography>
-        ) : (
-          <Typography variant="title" id="tableTitle">
-Purple Rock Scissors          </Typography>
-        )}
-      </div>
-      <div className={classes.spacer} />
-      
-    </Toolbar>
-  );
-};
-
-EnhancedTableToolbar.propTypes = {
-  classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
-};
-
-EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
 
 const styles = theme => ({
   root: {
-    width: '100%',
+    width: '84vw',
     marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+    height: '500px'
   },
   table: {
-    minWidth: 1020,
-  },
-  tableWrapper: {
-    overflowX: 'auto',
-    height: '84vh'
+    minWidth: 700,
   },
 });
 
-class KeywordOverview extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selected: [],
-      data: [
-        createData('Totals', 305, 3.7, 67, 4.3, 187),
-        createData('Avg.', 452, 25.0, 51, 4.9, .1233),
-        createData('Top 3 Totals', 262, 16.0, 24, 6.0, .8856),
-        createData('4-10 Totals', 159, 6.0, 24, 4.0, .02),
-        createData('11-20 Totals', 356, 16.0, 49, 3.9, .0235),
-        createData('21-50 Totals', 408, 3.2, 87, 6.5, .0674),
-        createData('51-100 Totals', 237, 9.0, 37, 4.3, .0012),
-        createData('Unbranded', 375, 0.0, 94, 0.0, .1156),
-        createData('Branded', 518, 26.0, 65, 7.0, .8821),
-      ],
-      page: 0,
-      rowsPerPage: 9,
-    };
-  }
-
-  handleRequestSort = (event, property) => {
-    const orderBy = property;
-    let order = 'desc';
-
-    if (this.state.orderBy === property && this.state.order === 'desc') {
-      order = 'asc';
-    }
-
-    this.setState({ order, orderBy });
-  };
-
-  handleSelectAllClick = (event, checked) => {
-    if (checked) {
-      this.setState(state => ({ selected: state.data.map(n => n.id) }));
-      return;
-    }
-    this.setState({ selected: [] });
-  };
-
-  handleClick = (event, id) => {
-    const { selected } = this.state;
-    const selectedIndex = selected.indexOf(id);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, id);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-
-    this.setState({ selected: newSelected });
-  };
-
-  handleChangePage = (event, page) => {
-    this.setState({ page });
-  };
-
-  handleChangeRowsPerPage = event => {
-    this.setState({ rowsPerPage: event.target.value });
-  };
-
-  isSelected = id => this.state.selected.indexOf(id) !== -1;
-
-  render() {
-    const { classes } = this.props;
-    const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
-
-    return (
-      
-      
-      <Paper className={classes.root} style={{width: '84vw',padding: '20px'}}>
-<div id="table1">
-        <div className={classes.tableWrapper}>
-          <Table className={classes.table} aria-labelledby="tableTitle">
-          <br></br><br></br><h1 style= {{fontSize: '14px', fontFamily: 'Montserrat', fontWeight: '500'}}>Keyword Overview</h1><br></br><br></br> 
-            <TableBody>
-              {data
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map(n => {
-                  const isSelected = this.isSelected(n.id);
-                  return (
-                    <TableRow
-                      hover
-                      onClick={event => this.handleClick(event, n.id)}
-                      role="checkbox"
-                      aria-checked={isSelected}
-                      tabIndex={-1}
-                      key={n.id}
-                      selected={isSelected}
-                    >
-                      <TableCell component="th" scope="row" padding="none">
-                        {n.name}
-                      </TableCell>
-                      <TableCell numeric>{n.calories}</TableCell>
-                      <TableCell numeric>{n.fat}</TableCell>
-                      <TableCell numeric>{n.carbs}</TableCell>
-                      <TableCell numeric>{n.protein}</TableCell>
-                      <TableCell numeric>{n.protein}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow style={{ height: 49 * emptyRows }}>
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-
-
-
-
-          
-
-          <Table className={classes.table} aria-labelledby="tableTitle">
-            <EnhancedTableHead
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={this.handleSelectAllClick}
-              onRequestSort={this.handleRequestSort}
-              rowCount={data.length}
-            />
-            <TableBody>
-              {data
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map(n => {
-                  const isSelected = this.isSelected(n.id);
-                  return (
-                    <TableRow
-                      hover
-                      onClick={event => this.handleClick(event, n.id)}
-                      role="checkbox"
-                      aria-checked={isSelected}
-                      tabIndex={-1}
-                      key={n.id}
-                      selected={isSelected}
-                    >
-                      <TableCell component="th" scope="row" padding="none">
-                        {n.name}
-                      </TableCell>
-                      <TableCell numeric>{n.calories}</TableCell>
-                      <TableCell numeric>{n.fat}</TableCell>
-                      <TableCell numeric>{n.carbs}</TableCell>
-                      <TableCell numeric>{n.protein}</TableCell>
-                      <TableCell numeric>{n.protein}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow style={{ height: 49 * emptyRows }}>
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table> 
-        </div>
-        </div>
-      </Paper>
-  
-
-    );
-  }
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
 }
 
-KeywordOverview.propTypes = {
+const data = [
+  createData('Totals', 159, 6.0, 24, 4.0),
+  createData('Avg.', 237, 9.0, 37, 4.3),
+  createData('Top 3 Totals', 262, 16.0, 24, 6.0),
+  createData('4-10 Totals', 305, 3.7, 67, 4.3),
+  createData('11-20 Totals', 356, 16.0, 49, 3.9),
+  createData('21-50 Totals', 356, 16.0, 49, 3.9),
+  createData('51-100 Totals', 356, 16.0, 49, 3.9),
+  createData('Unbranded', 356, 16.0, 49, 3.9),
+  createData('Branded', 356, 16.0, 49, 3.9)
+
+];
+
+function SimpleTable(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+        <br></br><br></br><h1 style= {{fontSize: '20px', fontFamily: 'Montserrat', fontWeight: '500', paddingLeft: '20px', color: '#534FC3'}}>Keyword Overview</h1><br></br><br></br>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell style= {{fontSize: '12px', fontWeight: '800'}}>Rank Count</TableCell>
+            <TableCell numeric style= {{fontSize: '12px', fontWeight: '800'}}>Search Volume Sum</TableCell>
+            <TableCell numeric style= {{fontSize: '12px', fontWeight: '800'}}>Avg. Competition</TableCell>
+            <TableCell numeric style= {{fontSize: '12px', fontWeight: '800'}}>Unique URL Density</TableCell>
+            <TableCell numeric style= {{fontSize: '12px', fontWeight: '800'}}>Traffic %</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(n => {
+            return (
+              <TableRow key={n.id}>
+                <TableCell component="th" scope="row" style= {{fontSize: '12px'}}>
+                  {n.name}
+                </TableCell>
+                <TableCell numeric style= {{fontSize: '12px'}}>{n.calories}</TableCell>
+                <TableCell numeric style= {{fontSize: '12px'}}>{n.fat}</TableCell>
+                <TableCell numeric style= {{fontSize: '12px'}}>{n.carbs}</TableCell>
+                <TableCell numeric style= {{fontSize: '12px'}}>{n.protein}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+SimpleTable.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(KeywordOverview);
+export default withStyles(styles)(SimpleTable);
